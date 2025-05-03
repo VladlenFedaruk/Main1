c7e3ccba1da2e1fc0e9a025dfe430d5f17d2dd9b
VladlenFedoruk <vladikttt@gmail.com>
2025-05-04 02:08:37 +0300

    Лесенка готова

---
+++ src/Lesenka.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Lesenka {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("ВВедите число");
+        int Les = scanner.nextInt();
+        int Les1 = Les;
+        for (int i=1; i<=Les; i++){
+            Les1--;
+        for (int j=1; j<=(Les1); j++){
+                System.out.print("#");
+            }
+            System.out.println("#");
+        }
+    }
+}
