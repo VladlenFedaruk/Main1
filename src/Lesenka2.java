c75679a09b201f0fa6da5b4de8038953037bf8d5
VladlenFedoruk <vladikttt@gmail.com>
2025-05-04 03:21:50 +0300

    Лесенка рекурсия

---
+++ src/Lesenka2.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Lesenka2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("введите число N");
+        int n = scanner.nextInt();
+        lesenka(n);}
+        public static int lesenka(int n){
+            System.out.println();
+            if (n<1)
+                return 1 ;
+            else {
+                for (int i=1; i<=n; i++){
+                System.out.print("#");
+                }
+                int h = lesenka(n-1);
+                return h;
+
+            }
+        }
+        }
+
+
+
