6e06b573480d0e3512dfe27ed439950da9468408
VladlenFedoruk <vladikttt@gmail.com>
2025-05-12 01:29:48 +0300

    Превернутый текс готов

---
+++ src/Lesson4/Task2.java
@@ -0,0 +1,12 @@
+package Lesson4;
+
+import java.util.Scanner;
+
+public class Task2 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("введите строку");
+        String text = scanner.nextLine();
+    }
+}
