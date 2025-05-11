c74d65a56a5c5e696b9024c67d6f9ad32bcef2d2
VladlenFedoruk <vladikttt@gmail.com>
2025-05-12 00:56:51 +0300

    Превернутый текс готов

---
+++ src/Lesson4/Task1.java
@@ -0,0 +1,31 @@
+package Lesson4;
+
+import java.util.Scanner;
+
+public class Task1 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("введите строку");
+        String text = scanner.nextLine();
+        StringBuilder result = new StringBuilder(text);
+        StringBuilder result2 = new StringBuilder();
+        result.reverse();
+        int countSymbol = text.length();
+        int k = 0;
+        for (int i = 0; i < countSymbol; i++) {
+            char symbol = text.charAt(i);
+            if (symbol != 32) {
+                int j = i - k;
+                result2.insert(j, symbol);
+            } else {
+                k = i + 1;
+                result2.insert(0, symbol);
+            }
+            System.out.println(result2);
+        }
+
+
+    }
+
+}
