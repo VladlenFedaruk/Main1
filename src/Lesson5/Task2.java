e906145a11bb0e34c8e9563598867538b6a8ca5a
VladlenFedoruk <vladikttt@gmail.com>
2025-05-12 23:10:31 +0300

    Готова

---
+++ src/Lesson5/Task2.java
@@ -0,0 +1,30 @@
+package Lesson5;
+
+import java.util.Scanner;
+
+public class Task2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("введите число элементов массива");
+        int n = scanner.nextInt();
+        int[] numbers = new int[n];
+        int j = 0;
+        int chet = 0;
+        int neChet = 0;
+
+        for (int i = 0; i < n; i++) {
+            j = i + 1;
+            System.out.println("введите " + j + " элемент массива");
+            numbers[i] = scanner.nextInt();
+            if (numbers[i] % 2 == 0) {
+                chet += 1;
+            }
+            if (numbers[i] % 2 != 0) {
+                neChet += 1;
+            }
+        }
+
+        System.out.println("четных чисел " + chet + "   нечетных чисел " + neChet);
+    }
+
+}
