86f02d1a98645be7d6c2dced279dda7f0dc54af0
VladlenFedoruk <vladikttt@gmail.com>
2025-06-02 03:59:59 +0300

    Готова

--- src/Lesson9/Main.java
+++ src/Lesson9/Main.java
@@ -7,16 +7,16 @@ public class Main {
 
     public static void main(String[] args) {
         Armor plateArmor = new Armor("Plate Armor", 300);
-        Armor robberJacket = new Armor("Robber Jacket", 200);
-        Armor huntsmanClothes = new Armor("Huntsman Clothes'", 100);
+        Armor robberJacket = new Armor("Robber Jacket", 260);
+        Armor huntsmanClothes = new Armor("Huntsman Clothes'", 200);
 
-        Weapon bastardSword = new Weapon("Bastard Sword", 50);
-        Weapon doubleDagger = new Weapon("Double Dagger", 75);
-        Weapon compoundBow = new Weapon("Compound Bow", 100);
+        Weapon bastardSword = new Weapon("Bastard Sword", 200);
+        Weapon doubleDagger = new Weapon("Double Dagger", 260);
+        Weapon compoundBow = new Weapon("Compound Bow", 300);
 
-        Player paladin = new Player("Paladin", bastardSword.baseWeaponDamage, plateArmor.baseArmor, 300);
-        Player archer = new Player("Archer", compoundBow.baseWeaponDamage, huntsmanClothes.baseArmor, 100);
-        Player robber = new Player("Robber", doubleDagger.baseWeaponDamage, robberJacket.baseArmor, 100);
+        Player paladin = new Player("Paladin", bastardSword.baseWeaponDamage, plateArmor.baseArmor, 3000);
+        Player archer = new Player("Archer", compoundBow.baseWeaponDamage, huntsmanClothes.baseArmor, 1000);
+        Player robber = new Player("Robber", doubleDagger.baseWeaponDamage, robberJacket.baseArmor, 2000);
         Player[] players = new Player[2];
 
         int j = 1;
@@ -42,6 +42,7 @@ public class Main {
             ch = scanner.nextLine();
             j++;
         }
+
 
     }
 }
