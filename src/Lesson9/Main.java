123bebd67b4f66fa2b55f0e88d15c7c0a207fe8c
VladlenFedoruk <vladikttt@gmail.com>
2025-06-02 17:00:13 +0300

    Готова

--- src/Lesson9/Main.java
+++ src/Lesson9/Main.java
@@ -14,15 +14,15 @@ public class Main {
         Weapon doubleDagger = new Weapon("Double Dagger", 260);
         Weapon compoundBow = new Weapon("Compound Bow", 300);
 
-        Player paladin = new Player("Paladin", bastardSword.baseWeaponDamage, plateArmor.baseArmor, 3000);
-        Player archer = new Player("Archer", compoundBow.baseWeaponDamage, huntsmanClothes.baseArmor, 1000);
-        Player robber = new Player("Robber", doubleDagger.baseWeaponDamage, robberJacket.baseArmor, 2000);
+        Player paladin = new Player("Paladin", bastardSword.baseWeaponDamage, plateArmor.baseArmor, 1000);
+        Player archer = new Player("Archer", compoundBow.baseWeaponDamage, huntsmanClothes.baseArmor, 800);
+        Player robber = new Player("Robber", doubleDagger.baseWeaponDamage, robberJacket.baseArmor, 600);
         Player[] players = new Player[2];
 
         int j = 1;
         Scanner scanner = new Scanner(System.in);
         for (int i = 0; i <= 1; i++) {
-            System.out.println("Игрок " + j + " выберете игрового персонажа  - Paladin, 2 - Archer, 3- Robber");
+            System.out.println("Игрок " + j + " выберете игрового персонажа  1- Paladin, 2 - Archer, 3- Robber");
             int num = scanner.nextInt();
             if (num == 1) {
                 players[i] = paladin;
@@ -35,18 +35,29 @@ public class Main {
             }
             players[i].getPlayerName();
             String ch = scanner.nextLine();
-            System.out.println("Ваши базовые характеристики " + players[i]);
+            System.out.println("Игрок " + players[i].playerName +" ваши базовые характеристики " + players[i]);
             ch = scanner.nextLine();
             players[i].getBonus();
-            System.out.println("Ваши характеристики после улучшения" + players[i]);
+            System.out.println("Игрок " + players[i].playerName +" ваши характеристики после улучшения " + players[i]);
             ch = scanner.nextLine();
             j++;
         }
         System.out.println("Ну что, сразимся?");
         String ch = scanner.nextLine();
-        players[0].getDamage();
-        int setDam = players[0].damage;
-        players[1].setDamage(setDam);
-        System.out.println(players[1]);
+        int i =0;
+        int l =1;
+        int k =0;
+        while ((players[i].baseHealth > 0) & (players[l].baseHealth > 0)) {
+            players[i].getDamage();
+            int setDam = players[i].damage;
+            players[l].setDamage(setDam);
+            System.out.println(players[l]);
+            k=i;
+            i=l;
+            l=k;
+        }
+        System.out.println(players[i].playerName + " вы лузер))))");
+        System.out.println(players[l].playerName + " вы вы ультрапобедитель))))");
+        System.out.println(players[l] + " смотрите как вы хороши, еще бы раз сложили))))");
     }
 }
--- src/Lesson9/Player.java
+++ src/Lesson9/Player.java
@@ -29,44 +29,47 @@ public class Player extends Character {
 
     public Player getBonus() {
         Player player = new Player(name, weapon, armor, baseHealth);
-        System.out.println("Нажмите интер чтобы бросить кубик и расчитать бонус на урон");
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Игрок " + playerName + " нажмите интер чтобы бросить кубик и расчитать бонус на урон");
         String ch = scanner.nextLine();
         int randomNum = ThreadLocalRandom.current().nextInt(1, 7);
         weapon = weapon + randomNum * 20;
         System.out.println("Оружие получает + " + randomNum * 10 + " к урону");
         ch = scanner.nextLine();
-        System.out.println("Нажмите интер чтобы бросить кубик и расчитать бонус к броне");
+        System.out.println("Игрок " + playerName + " нажмите интер чтобы бросить кубик и расчитать бонус к броне");
         ch = scanner.nextLine();
         randomNum = ThreadLocalRandom.current().nextInt(1, 7);
         armor = armor + randomNum * 10;
         System.out.println("Броня получает + " + randomNum * 10 + " к защите");
         ch = scanner.nextLine();
-        System.out.println("Нажмите интер чтобы бросить кубик и расчитать бонус к здоровью");
+        System.out.println("Игрок " + playerName + " нажмите интер чтобы бросить кубик и расчитать бонус к здоровью");
         ch = scanner.nextLine();
         randomNum = ThreadLocalRandom.current().nextInt(1, 7);
-        baseHealth = baseHealth + randomNum * 100;
+        baseHealth = baseHealth + randomNum * 10;
         System.out.println("Вы получаете + " + randomNum * 10 + " к здоровью");
         ch = scanner.nextLine();
         return player;
     }
 
     public void setDamage(int setDam) {
-        baseHealth = baseHealth - (setDam - armor);
-        System.out.println("Игрок "+ playerName +"  теряет " + (setDam - armor) + " здоровья "+ armor +" урона заблокировано броней");
+        if (setDam - armor > 0) {
+            baseHealth = baseHealth - (setDam - armor);
+        }
+
+        System.out.println("Игрок " + playerName + "  теряет " + (setDam - armor) + " здоровья " + armor + " урона заблокировано броней");
         Scanner scanner = new Scanner(System.in);
         String ch = scanner.nextLine();
     }
 
     public int getDamage() {
-        System.out.println("Игрок "+ playerName +" нажмите интер чтобы бросить кубик и расчитать бонус на урон");
+        System.out.println("Игрок " + playerName + " нажмите интер чтобы бросить кубик и расчитать бонус на урон");
         Scanner scanner = new Scanner(System.in);
         String ch = scanner.nextLine();
         int randomNum = ThreadLocalRandom.current().nextInt(1, 7);
         if (randomNum < 2) {
             damage = weapon;
         } else damage = weapon + 10 * randomNum;
-        System.out.println("Игрок "+playerName+" наносит "+damage+" урона " + weapon+ " урона от оружия "+ (damage-weapon)+ " критического урона");
+        System.out.println("Игрок " + playerName + " наносит " + damage + " урона " + weapon + " урона от оружия " + (damage - weapon) + " критического урона");
         ch = scanner.nextLine();
         return damage;
     }
